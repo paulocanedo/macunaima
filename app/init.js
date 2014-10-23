775670be84f0baff18e23a7cfecc8b10d6e5cbe2
Paulo Canedo <paulocanedo@gmail.com>
2014-10-23 13:40:14 -0300

    first skelleton of interface partial made

--- app/init.js
+++ app/init.js
@@ -1,3 +1,29 @@
-(function() {
-	angular.module('macunaima', ["mobile-angular-ui"]);
-})();
+var app = angular.module('macunaima', [
+  "ngRoute",
+  "mobile-angular-ui"
+  ]);
+
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider.when('/artists',   {templateUrl: "artists.htm", controller: 'ArtistController'}); 
+  $routeProvider.when('/albums',    {templateUrl: "albums.htm", controller: 'AlbumController'}); 
+  $routeProvider.when('/musics',    {templateUrl: "musics.htm"}); 
+  $routeProvider.otherwise({ redirectTo: '/artists'});
+});
+
+app.controller('MainController', function ($scope, $http) {
+  $scope.setTitleLabel = function(title) {
+    document.getElementById("app_title_label").innerHTML = title;
+  };
+});
+
+app.controller('AlbumController', function ($scope, $http) {
+  $scope.albums = DataService.getAlbums().albums;
+
+  $scope.setTitleLabel("Álbuns");
+});
+
+app.controller('ArtistController', function ($scope, $http) {
+  $scope.artists = DataService.getArtists().artists;
+
+  $scope.setTitleLabel("Artistas");
+});
